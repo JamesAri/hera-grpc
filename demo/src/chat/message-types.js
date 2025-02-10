22e6c07db2f4cd25ac9ec4140402a904d3f976b1
JamesAri <James.Ari@outlook.com>
2025-02-10 14:00:53 +0100

    separated demos into repos

--- demo/src/chat/message-types.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	AUTH: 'AUTH', // authentication message
-	CHAT: 'CHAT', // send a message to all users (if authenticated)
-}
--- demo/src/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	connection: {
-		host: 'localhost',
-		port: 50054,
-	},
-	zookeeper: 'zk://localhost:2181/hera-test',
-}
-
-
