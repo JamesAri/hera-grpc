553998ff2ab68b7454ba5bd82c043a86d7abf232
JamesAri <James.Ari@outlook.com>
2025-02-23 10:58:00 +0100

    improved naming and lib export

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ if (process.env.EXPERIMENTAL_OTEL) {
 	require('./experimental/tracing')
 }
 
+// TODO: make @grpc-js a peer dependency?
+const grpc = require('@grpc/grpc-js')
+
 const ServiceClient = require('./lib/client')
+const { COMPRESSION_ALGORITHMS, COMPRESSION_LEVELS } = require('./lib/constants')
 
-module.exports = ServiceClient
-
-module.exports.grpc = require('@grpc/grpc-js')
+module.exports = {
+	ServiceClient,
+	grpc,
+	compression: {
+		COMPRESSION_ALGORITHMS,
+		COMPRESSION_LEVELS,
+	},
+}
