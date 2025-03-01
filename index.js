13c94ccc73611db13996da3acf3b58166e8e9649
JamesAri <James.Ari@outlook.com>
2025-03-01 17:59:49 +0100

    lib package reflecting const refactoring

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 if (process.env.EXPERIMENTAL_OTEL) {
-	require('./experimental/tracing')
+	require('./experimental/instrumentation')
 }
 
 // TODO: make @grpc-js a peer dependency?
 const grpc = require('@grpc/grpc-js')
 
 const ServiceClient = require('./lib/client')
-const { compression } = require('./lib/constants')
-const INTERNAL_SERVICES = require('./lib/proto/internal-services')
+const compression = require('./lib/const/compression')
+const logLevels = require('./lib/const/log-levels')
+const rpcWhitelist = require('./lib/const/rpc-no-auth')
+const internal = require('./lib/proto/internal-services')
 
 module.exports = {
 	ServiceClient,
+	internal,
 	grpc,
 	compression,
-	internal: INTERNAL_SERVICES,
+	logLevels,
+	rpcWhitelist,
 }
